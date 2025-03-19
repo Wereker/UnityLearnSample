d6b2f65a8cc11469e8b5c445e9eeb43e3e80aa1d
Wereker <werkermatvey@gmail.com>
2025-03-20 00:20:08 +0700

    Добавлен скрипт SoftRemoveScript.cs

---
+++ Assets/Scripts/CustomScripts/SoftRemoveScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SoftRemoveScript : SampleSript
+{
+    [SerializeField]
+    private Transform target;
+    private float scale = 5f;
+    private float duration = 3f;
+
+    [ContextMenu("Активировать")]
+    public override void Use()
+    {
+        StartCoroutine(ResizeAndDestroy(target.localScale / scale));
+    }
+
+    public IEnumerator ResizeAndDestroy(Vector3 endSize)
+    {
+        float timer = 0;
+        Vector3 startSize = target.localScale;
+
+        Transform[] children = new Transform[target.childCount];
+        for (int i = 0; i < target.childCount; i++)
+        {
+            children[i] = target.GetChild(i);
+        }
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            foreach (Transform child in children)
+            {
+                if (child != null)
+                    child.localScale = Vector3.Lerp(startSize, endSize, timer / duration);
+            }
+
+            yield return null;
+        }
+
+        foreach (Transform child in children)
+        {
+            if (child != null) Destroy(child.gameObject);
+        }
+    }
+}
